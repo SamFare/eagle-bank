6e2cbc7f7be08c55e0f3ff7963bced4ade94e001
Sam Fare <samuel.fare@gmail.com>
2025-06-07 21:41:25 +0100

    implemented post/ users

--- src/__tests__/users.test.js
+++ src/__tests__/users.test.js
@@ -1,11 +1,15 @@
 const request = require('supertest');
+const { validate: uuidValidate } = require('uuid');
 const app = require('../app');
+
+let userData = {};
 
 describe('User Endpoints', () => {
     describe('POST /v1/users', () => {
-        it('creates a new user successfully', async () => {
-            const userData = {
-                name: 'Test User',
+        
+        beforeEach(() => { 
+             userData = {
+                name: Math.random(1, 1000000).toString(),
                 address: {
                     line1: '123 Test St',
                     town: 'Testville',
@@ -15,6 +19,39 @@ describe('User Endpoints', () => {
                 phoneNumber: '+441234567890',
                 email: 'test@example.com'
             };
+        })
+
+        it('creates a new user successfully', async () => {
+            const response = await request(app)
+                .post('/v1/users')
+                .set('Authorization', 'Bearer test-token')
+                .send(userData);
+
+            expect(response.status).toBe(201);
+            expect(response.body.name).toBe(userData.name)
         });
     });
+
+    it('Adds a UUID as the ID for a user', async () => { 
+        const response = await request(app)
+        .post('/v1/users')
+        .set('Authorization', 'Bearer test-token')
+        .send(userData);
+
+        expect(response.status).toBe(201);
+        expect(response.body.id).toBeTruthy()
+        expect(uuidValidate(response.body.id)).toBe(true)        
+    }); 
+
+    it('Adds a created and updated timestamp to the response', async () => { 
+            const response = await request(app)
+            .post('/v1/users')
+            .set('Authorization', 'Bearer test-token')
+            .send(userData);
+
+            expect(response.status).toBe(201);
+            expect(response.body.createdTimestamp).toBeTruthy()
+            expect(response.body.updatedTimestamp).toBeTruthy()
+
+    }); 
 });
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
 const router = express.Router();
 
 router.post('/v1/users', (req, res) => {
     res.status(201).json({
-        id: 'usr-123abc',
+        id: uuidv4(),
         name: req.body.name,
         address: req.body.address,
         phoneNumber: req.body.phoneNumber,
